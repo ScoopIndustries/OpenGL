45273f4691217038e6418fc68f80b37d77a134e0
ScoopIndustries <91889058+ScoopIndustries@users.noreply.github.com>
2022-03-10 12:25:09 +0100

    Cube

--- main.cpp
+++ main.cpp
@@ -1,189 +1,177 @@
-#include <cstdio>
-#include <iostream>
-#include <ratio>
-#include <chrono>
+#include<cstdio>
 #ifdef _WIN32
-#include <Windows.h>
-#endif // _WIN32
+	#include <windows.h>
+#endif
 
-#include <glm/glm.hpp>
-#include <glm/ext.hpp>
-#include <gl/GL.h>
-#include <SDL.h>
-#include <gl/GLU.h>
+#include<SDL.h>
+#include<SDL_opengl.h>
 
-#include "gc_3d_defs.hpp"
-using namespace std;
+#include<gl/GL.h>
+#include"gc_3d_defs.hpp"
 
-/// <summary>
-/// Camera Free / Rotation around Object
-/// </summary>
-
-
-	class APP {
-	public:
-		const float cameraSpeed = .5f;
-		//Camera Position
-		glm::vec3 cameraPosition;
-		//Camera Direction
-		glm::vec3 cameraTarget;
-		glm::vec3 cameraDirection;
-		//Right Axis
-		glm::vec3 up;
-		glm::vec3 cameraRight;
-		//UpAxis
-		glm::vec3 cameraUp;
-		//Camera Front
-		glm::vec3 cameraFront;
-
-		float camX;
-		float camZ;
-
-		APP()
-		{
-			cameraPosition = glm::vec3(0.0f, 0.0f, 0.3f);
-			cameraTarget = glm::vec3(0.0f, 0.0f, -2.0f);
-			cameraDirection = glm::normalize(cameraPosition - cameraTarget);
-			up = glm::vec3(0.0f, 1.0f, 0.0f);
-			cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-			cameraUp = glm::cross(cameraDirection, cameraRight);
-			cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-		}
-
-		void InputHandler(bool& appRunning, SDL_Event& event, std::chrono::steady_clock::time_point& prevTime)
-		{
-			auto resultCloak = Cloak(prevTime);
-
-			switch (event.type) {
-			case SDL_QUIT:
-				appRunning = false;
-				break;
-			case SDL_KEYDOWN:
-				switch (event.key.keysym.sym)
-				{
-				case SDLK_ESCAPE:
-					appRunning = false;
-					break;
-				case SDLK_a:
-					camX = glm::sin(resultCloak) * cameraSpeed;
-					camZ = glm::cos(resultCloak) * cameraSpeed;
-					cameraPosition = glm::vec3(camX, -1.0, camZ);
-					break;
-				case SDLK_q:
-					cameraPosition -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-					break;
-				case SDLK_e:
-					camX = glm::sin(resultCloak) * cameraSpeed;
-					camZ = glm::cos(resultCloak) * cameraSpeed;
-					cameraPosition = glm::vec3(camX, 1.0, camZ);
-					break;
-				case SDLK_d:
-					cameraPosition += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-					break;
-				}
-
-			}
-		}
-
-		float Cloak(std::chrono::steady_clock::time_point& prevTime)
-		{
-			auto curTime = std::chrono::steady_clock::now();
-			std::chrono::duration<float> fTime = curTime - prevTime;
-			return fTime.count();
-		}
-
-	};
-
-	void DrawCube(float size) {
-		glBegin(GL_QUADS);
-		//Front
-		glColor3f(1.0, 0.0, 0.0);
-		glVertex3f(size / 2, size / 2, size / 2);
-		glVertex3f(-size / 2, size / 2, size / 2);
-		glVertex3f(-size / 2, -size / 2, size / 2);
-		glVertex3f(size / 2, -size / 2, size / 2);
-		//LEFT
-		glColor3f(0.0, 1.0, 0.0);
-		glVertex3f(-size / 2, size / 2, size / 2);
-		glVertex3f(-size / 2, size / 2, -size / 2);
-		glVertex3f(-size / 2, -size / 2, -size / 2);
-		glVertex3f(-size / 2, -size / 2, size / 2);
-		//Back
-		glColor3f(0.0, 0.0, 1.0);
-		glVertex3f(size / 2, size / 2, -size / 2);
-		glVertex3f(-size / 2, size / 2, -size / 2);
-		glVertex3f(-size / 2, -size / 2, -size / 2);
-		glVertex3f(size / 2, -size / 2, -size / 2);
-		//Right
-		glColor3f(1.0, 1.0, 0.0);
-		glVertex3f(size / 2, size / 2, -size / 2);
-		glVertex3f(-size / 2, size / 2, -size / 2);
-		glVertex3f(-size / 2, -size / 2, -size / 2);
-		glVertex3f(size / 2, -size / 2, -size / 2);
-		//Top
-		glColor3f(1.0, 0.0, 1.0);
-		glVertex3f(size / 2, size / 2, size / 2);
-		glVertex3f(-size / 2, size / 2, size / 2);
-		glVertex3f(-size / 2, size / 2, -size / 2);
-		glVertex3f(size / 2, size / 2, -size / 2);
-		//Bottom
-		glColor3f(1.0, 1.0, 1.0);
-		glVertex3f(size / 2, -size / 2, size / 2);
-		glVertex3f(-size / 2, -size / 2, size / 2);
-		glVertex3f(-size / 2, -size / 2, -size / 2);
-		glVertex3f(size / 2, -size / 2, -size / 2);
-		glEnd();
-	}
-	void Display() {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//glLoadIdentity();
-		glTranslatef(0.0, 0.0, -5.0);
-		DrawCube(1.0);
-
-	}
+using namespace GC_3D;
 
 	int main(int argc, char* argv[])
 	{
 		SDL_Init(SDL_INIT_VIDEO);
 
-		Uint32 windowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
+		uint32_t windowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
 
-		SDL_Window* window = SDL_CreateWindow("Motor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 768, windowFlags);
+		
 
-		SDL_GLContext context = SDL_GL_CreateContext(window);
-		SDL_GL_MakeCurrent(window, context);
+		SDL_Window* win = SDL_CreateWindow("Moteur",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			1024,
+			768,
+			windowFlags);
+
+		SDL_GLContext context = SDL_GL_CreateContext(win);
+		SDL_GL_MakeCurrent(win, context);
+
+		auto prevTime = Clock::now();
+
+		Geometry geo;
 
 		bool appRunning = true;
-		auto prevTime = std::chrono::steady_clock::now();
-
-		APP app;
 		while (appRunning)
 		{
 			SDL_Event curEvent;
 			while (SDL_PollEvent(&curEvent))
 			{
-				app.InputHandler(appRunning, curEvent, prevTime);
+                if (curEvent.type == SDL_QUIT)
+                {
+                    SDL_Quit();
+                }
+                else if (curEvent.key.keysym.sym == SDLK_ESCAPE)
+                {
+                    SDL_Quit();
+                }
 			}
-			//LookAt
-			glm::mat4 view;
-			view = glm::lookAt(app.cameraPosition, app.cameraTarget, glm::vec3(0.0, 1.0, 0.0));
-			glm::mat4 projMatrix = glm::perspective(50.0f, 1024.f / 768.f, 1.0f, 1000.f);
-			
 
-			glClearColor(0.0, 0.0, 0.0, 1.0);
-			glMatrixMode(GL_PROJECTION);
-			glLoadMatrixf(glm::value_ptr(projMatrix));
-			glMatrixMode(GL_MODELVIEW);
-			glLoadMatrixf(glm::value_ptr(view));
-			glEnable(GL_DEPTH_TEST);
-			Display();
-
-			glDepthMask(GL_TRUE);
-			
 			glViewport(0, 0, 1024, 768);
-			SDL_GL_SwapWindow(window);
+			glClearColor(0.0, 0.0, 0.0, 0.0);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			glEnable(GL_DEPTH_TEST);
+			glDepthMask(GL_TRUE);
 
-			
+
+
+			const float radius = 0.5;
+
+			auto curTime = Clock::now();
+			std::chrono::duration<float> fTime = curTime - prevTime;
+			float camX = sin(fTime.count()) * radius;
+			float camZ = cos(fTime.count()) * radius;
+
+			vec3 cameraTarget = vec3(0.0, 0.0, -2.0);
+
+			vec3 cameraPos = cameraTarget - 6.0f * vec3(camX, 0.2, camZ);
+
+			// Creation de la camera
+			mat4 view;
+			view = lookAt(cameraPos, //Position de la camera
+				cameraTarget, //Cible à regarder
+				vec3(0.0, 1.0, 0.0)); //position vertical
+			mat4 camFrustum = frustum(-1.0, 1.0, -1.0, 1.0, 1.0, 1000.0);
+
+			glMatrixMode(GL_PROJECTION);
+			glLoadMatrixf(&camFrustum[0][0]);
+
+			glMatrixMode(GL_MODELVIEW);
+			glLoadMatrixf(&view[0][0]);
+
+			glEnable(GL_LIGHTING);
+			glEnable(GL_LIGHT0);
+			glTranslatef(0.0, 0.0, -2.0);
+
+            //GLfloat ambientLightFull[] = { 0.0f, 1.0f, 1.0f, 0.0f };
+            //glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLightFull);
+            //float red[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+            //glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, red);
+            
+            geo.m_Pos = {
+                //Front
+                vec3(-1.0, 1.0, 1.0),
+                vec3(1.0, 1.0, 1.0),
+                vec3(1.0, -1.0, 1.0),
+                vec3(-1.0, -1.0, 1.0),
+                //Back
+                vec3(-1.0, 1.0, -1.0),
+                vec3(1.0, 1.0, -1.0),
+                vec3(1.0, -1.0, -1.0),
+                vec3(-1.0, -1.0, -1.0),
+                //Left
+                vec3(-1.0, 1.0, 1.0),
+                vec3(-1.0, 1.0, -1.0),
+                vec3(-1.0, -1.0, -1.0),
+                vec3(-1.0, -1.0, 1.0),
+                //Right
+                vec3(1.0, 1.0, 1.0),
+                vec3(1.0, 1.0, -1.0),
+                vec3(1.0, -1.0, -1.0),
+                vec3(1.0, -1.0, 1.0),
+                //Up
+                vec3(-1.0, 1.0, 1.0),
+                vec3(-1.0, 1.0, -1.0),
+                vec3(1.0, 1.0, -1.0),
+                vec3(1.0, 1.0, 1.0),
+                //Back
+                vec3(-1.0, -1.0, 1.0),
+                vec3(-1.0, -1.0, -1.0),
+                vec3(1.0, -1.0, -1.0),
+                vec3(1.0, -1.0, 1.0),
+            };
+
+            geo.m_Normals = {
+                //Front
+                vec3(0.0, 0.0, 1.0),
+                vec3(0.0, 0.0, 1.0),
+                vec3(0.0, 0.0, 1.0),
+                vec3(0.0, 0.0, 1.0),
+                //Back
+                vec3(0.0, 0.0, -1.0),
+                vec3(0.0, 0.0, -1.0),
+                vec3(0.0, 0.0, -1.0),
+                vec3(0.0, 0.0, -1.0),
+                //Left
+                vec3(-1.0, 0.0, 0.0),
+                vec3(-1.0, 0.0, 0.0),
+                vec3(-1.0, 0.0, 0.0),
+                vec3(-1.0, 0.0, 0.0),
+                //Right
+                vec3(1.0, 0.0, 0.0),
+                vec3(1.0, 0.0, 0.0),
+                vec3(1.0, 0.0, 0.0),
+                vec3(1.0, 0.0, 0.0),
+                //Up
+                vec3(0.0, 1.0, 0.0),
+                vec3(0.0, 1.0, 0.0),
+                vec3(0.0, 1.0, 0.0),
+                vec3(0.0, 1.0, 0.0),
+                //Back
+                vec3(0.0, -1.0, 0.0),
+                vec3(0.0, -1.0, 0.0),
+                vec3(0.0, -1.0, 0.0),
+                vec3(0.0, -1.0, 0.0),
+            };
+
+            geo.m_Indices = {
+            0, 1, 2, 0, 2, 3, //Front
+            4, 5, 6, 4, 6, 7, //Back
+            8, 9, 10, 8, 10, 11, //Left
+            12, 13, 14, 12, 14, 15, //Right
+            16, 17, 18, 16, 18, 19, //Up
+            20, 21, 22, 20, 22, 23 //Down
+            };
+
+			geo.Bind();
+			geo.Draw();
+
+
+			SDL_GL_SwapWindow(win);
 		}
+
+		printf("\nHello World !\n");
 		return 0;
 	}
