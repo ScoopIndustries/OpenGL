f0135907f2d5cced742257deb1f375ad2c509f1f
ScoopIndustries <91889058+ScoopIndustries@users.noreply.github.com>
2022-03-14 19:00:24 +0100

    Correction Transfert

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #endif
 #define STB_IMAGE_IMPLEMENTATION
 #include<SDL.h>
-#include <iostream>
-#include <filesystem>
+
 
 #include "../Header/stb_image.h"
 #include "../Header/camera.hpp"
@@ -54,7 +53,8 @@ int main(int argc, char* argv[])
 	glewInit();
     Shape shape;
     Buffer m_Buffer;
-	Shader ourShader = Shader("D:/WKLEIN/OpenGL/Shader/SimpleVertexShader.vertexshader", "D:/WKLEIN/OpenGL/Shader/SimpleFragmentShader.fragmentshader");
+	//Shader ourShader = Shader("D:/WKLEIN/OpenGL/Shader/SimpleVertexShader.vertexshader", "D:/WKLEIN/OpenGL/Shader/SimpleFragmentShader.fragmentshader");
+	Shader ourShader = Shader("D:/ProjetOPENGL/OpenGL/Shader/SimpleVertexShader.vertexshader", "D:/ProjetOPENGL/OpenGL/Shader/SimpleFragmentShader.fragmentshader");
 
     glEnable(GL_DEPTH_TEST);
 
@@ -99,7 +99,8 @@ int main(int argc, char* argv[])
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(false); // tell stb_image.h to flip loaded texture's on the y-axis.
     // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char* data = stbi_load("D:/WKLEIN/OpenGL/Sprite/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("D:/ProjetOPENGL/OpenGL/Sprite/container.jpg", &width, &height, &nrChannels, 0);
+    //unsigned char* data = stbi_load("D:/ProjetOPENGL/OpenGL/Sprite/container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -121,7 +122,8 @@ int main(int argc, char* argv[])
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load("D:/WKLEIN/OpenGL/Sprite/awesomeface.png", &width, &height, &nrChannels, 0);
+    data = stbi_load("D:/ProjetOPENGL/OpenGL/Sprite/awesomeface.png", &width, &height, &nrChannels, 0);
+    //data = stbi_load("D:/ProjetOPENGL/OpenGL/Sprite/awesomeface.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
